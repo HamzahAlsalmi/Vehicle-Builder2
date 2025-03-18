0d02473c725fb2bc065f17b2e5975f306b2e5a2b
Hamzah <hkalsalmi@hotmail.com>
2025-03-18 15:26:30 -0600

    Final working version of Vehicle Builder CLI

--- src/classes/Wheel.ts
+++ src/classes/Wheel.ts
@@ -1,22 +1,20 @@
 // Wheel class that defines the properties of a wheel
 class Wheel {
-  // Declare properties of the Wheel class using private access modifier
   private diameter: number;
   private tireBrand: string;
 
-  // Constructor for the Wheel class
   constructor(diameter: number = 18, tireBrand: string = "GoodYear") {
     this.diameter = diameter;
     this.tireBrand = tireBrand;
   }
 
-  // Getter methods for the properties of the Wheel class
-  get getDiameter(): number {
+  // Getter for diameter
+  getDiameter(): number {
     return this.diameter;
   }
 
-  // Setter method for the diameter property
-  get getTireBrand(): string {
+  // Getter for tireBrand
+  getTireBrand(): string {
     return this.tireBrand;
   }
 }
